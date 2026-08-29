6d4146f77df2c9cb4374c1afccb8d8e5a3ec1a2a
abidkhanweb <abidkhanweb@gmail.com>
2026-08-29 12:03:12 +0530

    Add Snack compatibility

---
+++ App.js
@@ -0,0 +1,12 @@
+import { ExpoRoot } from 'expo-router';
+import Head from 'expo-router/head';
+
+const context = require.context('./src/app', true);
+
+export default function App() {
+  return (
+    <Head.Provider>
+      <ExpoRoot context={context} location="/" />
+    </Head.Provider>
+  );
+}
